c714ed7002bbdfd6f724bab1a023904d33364b81
BBBBlarry <blarry.personal@gmail.com>
2019-05-16 21:36:53 -0700

    Debugging

--- drop_library/models/model_with_counting.py
+++ drop_library/models/model_with_counting.py
@@ -251,14 +251,16 @@ class NumericallyAugmentedQaNetImprovedCounting(Model):
             # Shape: (batch_size, # of words, 2)
             count_number_logits = self._count_number_predictor(encoded_words)
 
+            # Shape: (batch_size, # of words,2)
+            count_passage_mask = passage_mask.unsqueeze(-1).repeat(1, 1, 2)
 
             logger.info("count_number_logits shape")
             logger.info(count_number_logits.shape)
-            logger.info("passage_mask shape")
-            logger.info(passage_mask.shape)
+            logger.info("count_passage_mask shape")
+            logger.info(count_passage_mask.shape)
 
 
-            count_number_probs = masked_softmax(count_number_logits, passage_mask, dim = 1, memory_efficient = True)
+            count_number_probs = masked_softmax(count_number_logits, count_passage_mask, dim = 1, memory_efficient = True)
             # count_number_log_probs = torch.log(count_number_probs)
 
             # counting result
