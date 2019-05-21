759d3f846d62e1f1e06b5e21a26aa1b904e05895
BBBBlarry <blarry.personal@gmail.com>
2019-05-21 12:42:49 -0700

    Fixed data type

--- drop_library/models/model_with_counting.py
+++ drop_library/models/model_with_counting.py
@@ -480,13 +480,16 @@ class NumericallyAugmentedQaNetImprovedCounting(Model):
                     # negative because it negates later
                     log_marginal_likelihood_list.append(-count_mse_loss)
 
+                    # for accuracy metrics
+                    best_count_number = torch.round(best_count_number).long()
+
                     logger.info("MSE")
                     logger.info(count_mse_loss)
                     logger.info("Predicted")
                     logger.info(best_count_number)
                     logger.info("Expected")
                     logger.info(clamped_gold_counts)
-                   
+
 
                 else:
                     raise ValueError(f"Unsupported answering ability: {answering_ability}")
